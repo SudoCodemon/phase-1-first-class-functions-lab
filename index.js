773791ae4c6e71bd8fa698da11ad6b943975c37e
Daniel Yang <danyang0724@gmail.com>
2022-05-18 10:56:21 -0400

    commit tree 2eb9ce4fbff023c5ed9e4ce7ec4bd295ae0e67d6

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Code your solution in this file!
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+
+function returnFirstTwoDrivers(array) {
+    const newArray = array.slice(0,2);
+    return newArray;
+}
+
+function returnLastTwoDrivers(array) {
+    const newArray = array.slice(-2);
+    return newArray;
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(x) {
+    if (x === 5) {
+        return num => num * 5;
+    } else if (x === 2) {
+        return num => num * 2;
+    } else if (x === 3) {
+        return num => num * 3;
+    }
+}
+
+const fareDoubler = createFareMultiplier(2);
+const fareTripler = createFareMultiplier(3); 
+
+function selectDifferentDrivers(a, b) {
+    return b(a);
+}
